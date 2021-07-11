a4d3be73bc8755d2a020edf3b7becc47fd7809a3
Keith A. Lewis <keithalewis@live.com>
2021-07-11 19:30:35 -0400

    use blas::vector_alloc

--- xll_blas.cpp
+++ xll_blas.cpp
@@ -4,27 +4,90 @@
 using namespace xll;
 using namespace blas;
 
-AddIn xai_blas_vector(
-	Function(XLL_HANDLEX, "xll_blas_vector", "\\BLAS.VECTOR")
+#ifdef _DEBUG
+
+Auto<Open> xao_allocator_test([]() {
+	try {
+		xll::allocator<double> alloc;
+		double* p = alloc.allocate(3);
+		ensure(p != nullptr);
+		ensure(alloc.fp.size() == 3);
+		alloc.fp[0] = 1;
+
+		xll::allocator<double> alloc2{ alloc };
+		ensure(alloc2.fp.size() == 3);
+		ensure(alloc2.fp[0] == 1);
+
+		fpvector fp(3,2);
+		ensure(fp.size() == 3);
+		ensure(fp.capacity() == 6);
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+
+		return FALSE;
+	}
+
+
+	return TRUE;
+});
+
+#endif // _DEBUG
+
+AddIn xai_blas_vector_(
+	Function(XLL_HANDLEX, "xll_blas_vector_", "\\BLAS.VECTOR")
 	.Arguments({
-		Arg(XLL_LONG, "size", "is the size of the vector."),
-		Arg(XLL_LONG, "step", "is the stride of the vector. Default is 1."),
+		Arg(XLL_FPX, "vector", "is a vector or the size of the vector."),
+		Arg(XLL_LONG, "_incr", "is the optional stride of the vector. Default is 1."),
 		})
 	.Uncalced()
 	.FunctionHelp("Return a handle to a BLAS strided vector.")
 );
-HANDLEX WINAPI xll_blas_vector(LONG n, LONG dn)
+HANDLEX WINAPI xll_blas_vector_(const _FPX* pv, LONG dn)
 {
 #pragma XLLEXPORT
 	HANDLEX h = INVALID_HANDLEX;
 
 	try {
-		handle<blas::vector<double>> h_(new blas::vector_array<double>(n, dn));
-		h = h_.get();
+		if (dn != 0 and size(*pv) == 1) {
+			int n = static_cast<int>(pv->array[0]);
+			handle<blas::vector<double>> h_(new fpvector(n, dn));
+			h = h_.get();
+		}
+		else {
+			handle<blas::vector<double>> h_(new fpvector(size(*pv), &pv->array[0], 1));
+			h = h_.get();
+		}
 	}
 	catch (const std::exception& ex) {
 		XLL_ERROR(ex.what());
 	}
 
 	return h;
+}
+
+AddIn xai_blas_vector(
+	Function(XLL_FPX, "xll_blas_vector", "BLAS.VECTOR")
+	.Arguments({
+		Arg(XLL_HANDLEX, "handle", "is a handle to a vector."),
+		})
+	.FunctionHelp("Return BLAS vector.")
+);
+_FPX* WINAPI xll_blas_vector(HANDLEX h)
+{
+#pragma XLLEXPORT
+	_FPX* fpx = nullptr;
+
+	try {
+		handle<blas::vector<double>> h_(h);
+		ensure(h_);
+		const auto& v_ = h_.as<fpvector>();
+		
+		fpx = v_->alloc.fp.get();
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return fpx;
 }
--- xll_blas.h
+++ xll_blas.h
@@ -1,7 +1,33 @@
 // xll_blas.h - BLAS routines
 #pragma once
+#include <memory>
 #include "fms_blas/fms_blas.h"
 //#define XLOPERX XLOPER
 #include "xll/xll/xll.h"
 
 #define CATEGORY "BLAS"
+
+namespace xll {
+
+	template<class T>
+	struct allocator {
+		static_assert(std::is_same_v<T, double>);
+		using value_type = T;
+
+		FPX fp;
+
+		[[nodiscard]] T* allocate(std::size_t n)
+		{
+			fp.resize((int)n, 1);
+
+			return fp.begin();
+		}
+		void deallocate(T*, std::size_t)
+		{
+			fp.resize(0, 0);
+		}
+	};
+
+	using fpvector = blas::vector_alloc<double, xll::allocator<double>>;
+
+} // namepace xll
